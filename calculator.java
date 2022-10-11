220de6d5c4212a2f52d8b7cdd5a9552e0b793474
mrman439 <112532387+mrman439@users.noreply.github.com>
2022-10-10 19:35:02 -0700

    Update calculator.java

--- calculator.java
+++ calculator.java
@@ -1,25 +1,7 @@
 import java.util.Scanner;
+import static Calc_essentials.Calc_operators.*;
 
 public class Calculator {
-
-    public static String SentenceStarter = "The answer is: ";
-
-    public static void Add(float x, float y){
-        float answer = x + y;
-        System.out.print(SentenceStarter + answer);
-    }
-    public static void Minus(float x, float y){
-        float answer = x - y;
-        System.out.print(SentenceStarter + answer);
-    }
-    public static void Multiply(float x, float y){
-        float answer = x * y;
-        System.out.print(SentenceStarter + answer);
-    }
-    public static void Divide(float x, float y){
-        float answer = x / y;
-        System.out.print(SentenceStarter + answer);
-    }
 
     public static void main(String[] args) {
 
